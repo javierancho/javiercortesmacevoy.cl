05a37d381a37e07100b3f8fc0045b9728d19934b
javierancho <javiercortes.mac@gmail.com>
2026-06-28 04:21:42 -0400

    Update analytics endpoint URL

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -6,7 +6,7 @@
 ============================================================ */
 
 const JCMAnalytics = (function () {
-  const ENDPOINT = "https://script.google.com/macros/s/AKfycbwjBNfzTQdqImOrnmYhgknI2wVbqtIJFimOim7Uv7wAm6N66RSXxMFWz2gY1BiUGQQo/exec";
+  const ENDPOINT = "https://script.google.com/macros/s/AKfycbz9zlvNdIH2A5_OBH9tImljut65e9UihKq1XTuvyq_5meffup3R8sjyrRdFOAFUuRxg/exec";
 
   async function track(data) {
     try {
