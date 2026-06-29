f2363432ea119d31aa0f4fcf632fee472e7f72a9
javierancho <javiercortes.mac@gmail.com>
2026-06-29 17:19:39 -0400

    Initialize map and add marker management functions

--- herramientas/sismos/js/map.js
+++ herramientas/sismos/js/map.js
@@ -1,1 +1,72 @@
+/* ==========================================================
+   Monitor de Sismos
+   Gestión del mapa (Leaflet)
+========================================================== */
 
+let map;
+let markers = [];
+
+/**
+ * Inicializa el mapa
+ */
+function initMap() {
+
+    map = L.map("map").setView([-30.0, -71.0], 4);
+
+    L.tileLayer(
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+        {
+            attribution:
+                "&copy; OpenStreetMap contributors"
+        }
+    ).addTo(map);
+
+}
+
+/**
+ * Elimina todos los marcadores
+ */
+function clearMarkers() {
+
+    markers.forEach(marker => {
+
+        map.removeLayer(marker);
+
+    });
+
+    markers = [];
+
+}
+
+/**
+ * Agrega un marcador
+ */
+function addMarker(lat, lon, popup) {
+
+    const marker = L.marker([lat, lon]);
+
+    marker.bindPopup(popup);
+
+    marker.addTo(map);
+
+    markers.push(marker);
+
+}
+
+/**
+ * Centrar en Chile
+ */
+function centerChile() {
+
+    map.setView([-30.0, -71.0], 4);
+
+}
+
+/**
+ * Mostrar mundo
+ */
+function viewWorld() {
+
+    map.setView([15, 0], 2);
+
+}
